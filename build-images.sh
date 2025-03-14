40b93cbe064a3a62da95bd4291ae219e882ac008
mrmarkuz <mrmarkuz666@gmail.com>
2025-03-14 11:25:18 +0100

    Add accountconsumer role

--- build-images.sh
+++ build-images.sh
@@ -36,7 +36,7 @@ buildah add "${container}" imageroot /imageroot
 buildah add "${container}" ui/dist /ui
 
 buildah config --entrypoint=/ \
-    --label="org.nethserver.authorizations=node:fwadm traefik@node:routeadm" \
+    --label="org.nethserver.authorizations=node:fwadm traefik@node:routeadm cluster:accountconsumer" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
     --label="org.nethserver.images=docker.io/openvpn/openvpn-as:2.14.2-40b190d8-Ubuntu24" \
